22584ab9be61277fe271bf3cfc5a665031675f5e
ali_z <ali3.zargar16@gmail.com>
2024-01-05 16:36:05 +0330

    modify functions to hancle null inputs

--- main.cpp
+++ main.cpp
@@ -70,6 +70,11 @@ public:
     bool isAncestor(const string &ancestor, const string &descendant) // returns whether one is the ancestor of the other
     {
         TreeNode *Ancestor = findNode(ancestor, root);
+        if (Ancestor == nullptr)
+        {
+            cout << "Ancestor didn't exist!\n";
+            return NULL;
+        }
         TreeNode *Descendant = findNode(descendant, Ancestor);
         if (Descendant != nullptr)
             return true;
@@ -79,6 +84,11 @@ public:
     bool areSiblings(const string &person1, const string &person2) // returns whether they are siblings
     {
         TreeNode *Person1 = findNode(person1, root);
+        if (Person1 == nullptr)
+        {
+            cout << "First person didn't exist!\n";
+            return NULL;
+        }
         for (TreeNode *i : Person1->parent->children)
         {
             if (i->data == person2)
@@ -90,12 +100,22 @@ public:
     bool areCousins(const string &person1, const string &person2) // returns whether they are siblings
     {
         TreeNode *Person1 = findNode(person1, root);
+        if (Person1 == nullptr)
+        {
+            cout << "First person didn't exist!\n";
+            return NULL;
+        }
         for (TreeNode *i : Person1->parent->children)
         {
             if (i->data == person2)
                 return false;
         }
         TreeNode *Person2 = findNode(person2, root);
+        if (Person2 == nullptr)
+        {
+            cout << "Second person didn't exist!\n";
+            return NULL;
+        }
         if (Person1->depth == Person2->depth)
             return true;
         return false;
@@ -103,9 +123,20 @@ public:
 
     TreeNode *lowestCommonAncestor(const string &person1, const string &person2) // returns the lowest common ancestor
     {
-
-        TreeNode *Person1 = findNode(person1, root)->parent;
-        TreeNode *Person2 = findNode(person2, root)->parent;
+        TreeNode *Person1 = findNode(person1, root);
+        if (Person1 == nullptr)
+        {
+            cout << "First person didn't exist!\n";
+            return nullptr;
+        }
+        TreeNode *Person2 = findNode(person2, root);
+        if (Person2 == nullptr)
+        {
+            cout << "Second person didn't exist!\n";
+            return nullptr;
+        }
+        Person1 = Person1->parent;
+        Person2 = Person2->parent;
         while (Person1->depth != Person2->depth)
         {
             if (Person1->depth > Person2->depth)
@@ -125,10 +156,9 @@ public:
     void addNode(const string &parentValue, const string &childValue) // adds a child to a parent
     {
         TreeNode *parentNode = findNode(parentValue, root);
-
         if (parentNode == nullptr)
         {
-            cout << "ERROR! no such parent exists\n";
+            cout << "Parent didn't exist!\n";
             return;
         }
 
@@ -143,7 +173,7 @@ public:
 
         if (node == nullptr)
         {
-            cout << "ERROR! no such node exists\n";
+            cout << "The person didn't exist\n";
             return;
         }
         for (size_t i = 0; i < node->parent->children.size(); i++)
@@ -162,6 +192,9 @@ public:
 
     void printSubTree(TreeNode *root, int depth) // prints a sub tree
     {
+        if (root == nullptr)
+            return;
+
         for (int i = 0; i < depth; ++i)
         {
             cout << "--";
@@ -177,9 +210,13 @@ public:
 
     void createJson() // creates a json file of the tree in Breadth first order
     {
-        ofstream file("tree_data.json");
-        file << "{\n";
+        if (root == nullptr)
+            return;
+
         deque<TreeNode *> queue;
+        ofstream file("tree_data.json");
+
+        file << "{\n";
         queue.push_back(root);
         while (queue.size() > 0)
         {
